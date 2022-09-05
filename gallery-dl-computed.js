da4910b6059358adfeaf1e67e0468e6a9b63ae8c
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-09-05 22:17:21 +0300

    Update

--- gallery-dl-computed.js
+++ gallery-dl-computed.js
@@ -5,18 +5,35 @@ import json2 from "./jsons/safebooru-5615470.json" assert {type: "json"};
 
 // -------------
 // Assume it's in gallery-dl.conf
+/**
+ * @type {{
+ * customTags: {},
+ * ignore: string[],
+ * allTags: string[],
+ * tags: string[],
+ * [byteLimit]: number,
+ * [separator]: string,
+ * [splitter]: string,
+ * [joiner]: string,
+ * [deduplicate]: boolean,
+ * }}
+ */
 const computedTagLineSetting = {
-    "allTags": ["tags"],
-    "custom": {
-        "tags_important": ["third-party_edit", "sound_edit", "paid_reward"],
+    "customTags": {
+        "tags_important": {
+            "source": ["tags"],
+            "only": ["third-party_edit", "sound_edit", "paid_reward"],
+        },
+        "tags_important_general": {
+            "source": ["tags_general"],
+            "only": ["female_only"]
+        }
     },
-    "tags": ["tags_artist", "tags_important", "tags_character", "tags_copyright", "tags_studio", "tags_general", "tags_genre", "tags_medium", "tags_meta"],
-    "deduplicate": true,
-    "limit": 130,
-    "byteLimit": 130,
-    "separator": " ",
-    "splitter": " ",
-    "ignore": ["*filesize", "*resolution"]
+    "tags": ["tags_artist", "tags_important", "tags_character", "tags_important_general", "tags_copyright", "tags_studio", "tags_general", "tags_genre", "tags_medium", "tags_meta"],
+    "ignore": ["tagme", "cg_art", "game_cg", "artist_cg", "webm", "mp4", "video", "animated"],
+    "tags_meta": {
+        "ignore": ["*filesize", "*resolution", "*filesize", "*aspect_ratio"]
+    }
 };
 // -------------
 
@@ -26,15 +43,23 @@ const propsObject = {
 propsObject.computedTagLine = getComputedTagLine(computedTagLineSetting);
 
 function getComputedTagLine(settings = {}) {
-    let {tags, limit, byteLimit, separator, custom} = settings;
-    tags = tags || [];
-    limit = limit || 100;
-    separator = separator || " ";
+    const limit     = settings.limit     || 120;
+    const byteLimit = settings.byteLimit || 120;
+    const separator = settings.separator || " ";
+    const splitter  = settings.splitter  || " ";
+    const joiner    = settings.joiner    || " ";
+    const deduplicate = settings.deduplicate || true;
 
     const allTags = settings.allTags.map(name => propsObject[name] || []).flat();
-    tags = tags.map(name => propsObject[name] || (settings.custom[name] ? handleCustomTags(settings.custom[name]) : []));
+    const selectedTags = settings.tags || allTags || [];
+    const tags = selectedTags.map(name => propsObject[name] || handleCustomTags(name) || []);
 
-    function handleCustomTags(custom) {
+
+    function handleCustomTags(name) {
+        const custom = settings.customTags[name];
+        if (!custom) {
+            return;
+        }
         return custom.filter(tag => allTags.includes(tag));
     }
 
